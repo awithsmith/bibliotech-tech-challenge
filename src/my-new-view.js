ab66fce33f0dc0d1b85c471855167399dc9a47c6
Alex <awithsmith@gmail.com>
2018-09-28 11:26:19 +0100

    book jacket now displayed by iron-image in new view

--- src/my-new-view.js
+++ src/my-new-view.js
@@ -2,27 +2,19 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 /* Load shared styles. All view elements use these styles */
 import './shared-styles.js';
+import '@polymer/iron-image/iron-image.js';
 
 /* Extend the base PolymerElement class */
 class MyNewView extends PolymerElement {
   /* Define a template for the new element */
   static get template() {
     return html`
-      <style include="shared-styles">
-        :host {
-          display: block;
-
-          padding: 10px;
-        }
-      </style>
-
-      <div class="card">
-        <div class="circle">1</div>
-        <h1>New View</h1>
-        <p>New view!</p>
-      </div>
-    `;
+    <div class="card">
+           <iron-image src="https://d1re4mvb3lawey.cloudfront.net/pg1017/cover.jpg"></iron-image>
+         </div>
+      `;
+    }
   }
-}
+
 /* Register the new element with the browser */
 window.customElements.define('my-new-view', MyNewView);
